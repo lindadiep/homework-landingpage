b24abc46e1e89ba4b88d7773e72cb418679f6c5d
lindadiep <lindadiep@Diana-Dieps-MacBook-Pro.local>
2016-01-21 23:41:23 -0500

    Slide and hide working
    Was able to get blogpost1 to slide and hide successfully, however, need
    to code in a way so js is unique to each blogpost. Right now, what
    happens to one happens to the other.

--- submit/blog.js
+++ submit/blog.js
@@ -1,18 +1,19 @@
-$(document).ready( function() {
+$(document).ready( function(collapseBlogPost) {
 		
-		function collapseBlogPost() {
-			$('#expand-blog1').slideUp();
-			$('.read-less').hide();
+	function collapseBlogPost() {
+		$('.expand-blog').slideUp();
+		$('.read-more').show();
+		$('.read-less').hide();
 	}
+
+	$('.read-more').click(expandBlogPost); 
+
+		function expandBlogPost() {
+			$('.expand-blog').slideDown();
+			$('.read-less').show();
+			$('.read-more').hide();
+		}
+
+	$('.read-less').click(collapseBlogPost);
+
 });
-
-$('#read-more').click(expandBlogPost); 
-
-function expandBlogPost() {
-	$('#expand-blog1').slideDown();
-	$('.read-less').show();
-}
-
-$('read-less').click(collapseBlogPost);
-
-
