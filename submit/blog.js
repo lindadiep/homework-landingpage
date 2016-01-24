d7495ff3b3444239f9b65cbe6f84ec01957d41a3
lindadiep <lindadiep@Diana-Dieps-MacBook-Pro.local>
2016-01-23 23:32:28 -0500

    Tried to add js to learn-more
    It didn’t work! Will fix later.

--- submit/blog.js
+++ submit/blog.js
@@ -1,19 +1,24 @@
 $(document).ready( function(collapseBlogPost) {
-		
+
 	function collapseBlogPost() {
-		$('.expand-blog').slideUp();
+		$('.expand-firstpost').slideUp();
 		$('.read-more').show();
 		$('.read-less').hide();
 	}
 
-	$('.read-more').click(expandBlogPost); 
+	$('.read-more').click(expandBlogPost);
 
-		function expandBlogPost() {
-			$('.expand-blog').slideDown();
-			$('.read-less').show();
-			$('.read-more').hide();
-		}
+	function expandBlogPost() {
+		$('.expand-firstpost').slideDown();
+		$('.read-less').show();
+		$('.read-more').hide();
+	}
 
 	$('.read-less').click(collapseBlogPost);
+
+	$
+	function collapseLearnMore() {
+		$('.learn-more').
+	}
 
 });
