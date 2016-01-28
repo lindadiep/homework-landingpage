4da96eb034295ba696795b12cc62f593845bda9d
Nayana Davis <nayana487@gmail.com>
2016-01-28 15:27:41 -0500

    added comments

--- submit/blog.js
+++ submit/blog.js
@@ -1,5 +1,9 @@
 $(document).ready( function(collapseBlogPost, collapseLearnMore) {
-
+// ND: Take a look at this the documentation for how document.ready works: https://learn.jquery.com/using-jquery-core/document-ready/
+// This is the biggest issue here. The purpose is to make sure that the DOM fully loads before any
+// JS code runs. You don't put functions inside the parameters for this.
+// I think once you get this handled, your functions should be close to working.
+// You are definitely on the right track
 	function collapseBlogPost() {
 		$('.expand-firstpost').slideUp();
 		$('.read-more').show();
